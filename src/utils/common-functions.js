8cf96962384db43158b632617c6aa8ad16380620
Dinesh Shaw <49403734+dinezh256@users.noreply.github.com>
2020-05-23 18:45:33 +0530

    commaSeparated

--- src/utils/common-functions.js
+++ src/utils/common-functions.js
@@ -1,16 +1,16 @@
 const months = {
-  '01': 'Jan',
-  '02': 'Feb',
-  '03': 'Mar',
-  '04': 'Apr',
-  '05': 'May',
-  '06': 'Jun',
-  '07': 'Jul',
-  '08': 'Aug',
-  '09': 'Sep',
-  '10': 'Oct',
-  '11': 'Nov',
-  '12': 'Dec',
+  "01": "Jan",
+  "02": "Feb",
+  "03": "Mar",
+  "04": "Apr",
+  "05": "May",
+  "06": "Jun",
+  "07": "Jul",
+  "08": "Aug",
+  "09": "Sep",
+  "10": "Oct",
+  "11": "Nov",
+  "12": "Dec",
 };
 
 export const formatDate = (unformattedDate) => {
@@ -32,12 +32,12 @@ export const validateCTS = (data = []) => {
   const today = new Date();
   today.setHours(0, 0, 0, 0);
   const dataTypes = [
-    'dailyconfirmed',
-    'dailydeceased',
-    'dailyrecovered',
-    'totalconfirmed',
-    'totaldeceased',
-    'totalrecovered',
+    "dailyconfirmed",
+    "dailydeceased",
+    "dailyrecovered",
+    "totalconfirmed",
+    "totaldeceased",
+    "totalrecovered",
   ];
   return data
     .filter((d) => dataTypes.every((dt) => d[dt]) && d.date)
@@ -47,3 +47,14 @@ export const validateCTS = (data = []) => {
       return new Date(d.date + year) < today;
     });
 };
+
+export function commaSeperated(x) {
+  if (x !== undefined) {
+    x = x.toString();
+    let lastThree = x.substring(x.length - 3);
+    let otherNumbers = x.substring(0, x.length - 3);
+    if (otherNumbers !== "") lastThree = "," + lastThree;
+    let res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+    return res;
+  } else return 0;
+}
