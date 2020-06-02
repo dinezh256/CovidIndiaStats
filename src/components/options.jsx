61515e27be0605e9f2acfb2b4ed0e2bc62fde143
Dinesh Shaw <49403734+dinezh256@users.noreply.github.com>
2020-06-02 11:59:56 +0530

    helpline data added

--- src/components/options.jsx
+++ src/components/options.jsx
@@ -16,7 +16,7 @@ function Options() {
           className="question fadeInUp"
           style={{
             textTransform: "uppercase",
-            marginBottom: -15,
+            marginBottom: 5,
             animationDelay: "0.2s",
           }}
         >
@@ -27,26 +27,74 @@ function Options() {
             fill="rgba(128, 42, 190, 0.6)"
             stroke="rgb(128, 42, 190)"
           />{" "}
-          Cyclone Nisarga Helpline
+          Nisarga Helpline
+          <h6 className="cycloneDetails">
+            According to the India Meteorological Department (IMD), Nisarga is
+            likely to make landfall between Harihareshwar in Maharashtra’s
+            Raigad and Daman on 3rd June. The IMD has issued a Red alert for
+            other districts: Palghar, Thane, Dhule, Nandurbar and Nashik,
+            between 3rd and 4th June. In Gujarat, 10 teams of NDRF have taken
+            position in south Gujarat districts of Surat, Bharuch, Navsari,
+            Valsad and the Dangs and Bhavnagar and Amreli districts of
+            Saurashtra to deal with any eventuality.
+          </h6>
         </h4>
         <div className="options fadeInUp" style={{ animationDelay: "0.25s" }}>
-          <h4 className="question">
+          <h5 className="question">
             National Disaster Management Authority, Government Of India
-          </h4>
+          </h5>
           <span className="options-link">011-1078, 011-26701700</span>
         </div>
         <div className="options fadeInUp" style={{ animationDelay: "0.3s" }}>
-          <h4 className="question">Maharashtra Control Room</h4>
+          <h5 className="question">Daman Disaster Management Control Room</h5>
+          <span className="options-link">0260-2231377</span>
+          <span className="options-link">
+            <Icon.ExternalLink />{" "}
+            <a
+              href="https://daman.nic.in/state-disaster-managment-dd.aspx"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="options-link2"
+            >
+              daman.nic.in
+            </a>
+          </span>
+        </div>
+        <div className="options fadeInUp" style={{ animationDelay: "0.3s" }}>
+          <h5 className="question">
+            Maharashtra Disaster Management Control Room
+          </h5>
           <span className="options-link">022-22027990</span>
         </div>
         <div className="options fadeInUp" style={{ animationDelay: "0.45s" }}>
-          <h4 className="question">
+          <h5 className="question">
+            Maharashtra Districtwise Emergency Helpline
+          </h5>
+          <span className="options-link">Palghar: 0250-2332618</span>
+          <span className="options-link">Thane: 022-25821281,022-25827402</span>
+          <span className="options-link">Raigad: 108 / 1077</span>
+          <span className="options-link">Nandurbar: 02564-210077</span>
+          <span className="options-link">Nashik: 0253-2360855</span>
+          <span className="options-link">
+            <Icon.ExternalLink />{" "}
+            <a
+              href="https://mahadish.in/disaster_management"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="options-link2"
+            >
+              mahadish.in/disaster_management
+            </a>
+          </span>
+        </div>
+        <div className="options fadeInUp" style={{ animationDelay: "0.45s" }}>
+          <h5 className="question">
             Municipal Corporation of Greater Mumbai Emergency Operation Centre
             (Disaster Management Unit)
-          </h4>
+          </h5>
           <span className="options-link">
-            022-22694725, 022-22694727, 022-22704403 | <Icon.Mail />{" "}
-            ccrsdmp@gmail.com | <Icon.ExternalLink />{" "}
+            022-22694725/27, 022-22704403 | <Icon.Mail /> ccrsdmp@gmail.com |{" "}
+            <Icon.ExternalLink />{" "}
             <a
               href="http://dm.mcgm.gov.in"
               target="_blank"
@@ -57,35 +105,59 @@ function Options() {
             </a>
           </span>
         </div>
+        <div className="options fadeInUp" style={{ animationDelay: "0.3s" }}>
+          <h5 className="question">Gujarat Disaster Management Control Room</h5>
+          <span className="options-link">+91-79-23259283</span>
+        </div>
+        <div className="options fadeInUp" style={{ animationDelay: "0.45s" }}>
+          <h5 className="question">Gujarat Districtwise Emergency Helpline</h5>
+          <span className="options-link">Surat: 0261-2452807</span>
+          <span className="options-link">Bharuch: +91-97-17393310</span>
+          <span className="options-link">Navsari: 02637-259401</span>
+          <span className="options-link">Valsad: 02632-243238</span>
+          <span className="options-link">Dang: 02631-220346</span>
+          <span className="options-link">Bhavnagar: 0278-2521554</span>
+          <span className="options-link">Amreli: 8319460485</span>
+          <span className="options-link">
+            <Icon.ExternalLink />{" "}
+            <a
+              href="http://gsdma.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="options-link2"
+            >
+              gsdma.org
+            </a>
+          </span>
+        </div>
         <h4
           className="question fadeInUp"
           style={{
             textTransform: "uppercase",
             marginTop: 15,
-            marginBottom: -15,
+            marginBottom: 5,
             animationDelay: "0.55s",
           }}
         >
-          <Icon.Link
+          <Icon.Link2
             size={20}
-            strokeWidth={2.5}
+            strokeWidth={3}
             style={{ verticalAlign: "-0.1rem" }}
-            fill="rgba(128, 42, 190, 0.6)"
             stroke="rgb(128, 42, 190)"
           />{" "}
           Important Links
         </h4>
         <div className="options fadeInUp" style={{ animationDelay: "0.6s" }}>
-          <h4 className="question">
+          <h5 className="question">
             Ministry of Health and Family Welfare, Govt. of India
-          </h4>
+          </h5>
           <a href="https://www.mohfw.gov.in" className="options-link">
             mohfw.gov.in
           </a>
         </div>
 
         <div className="options fadeInUp" style={{ animationDelay: "0.65s" }}>
-          <h4 className="question">WHO : Covid19 Home Page</h4>
+          <h5 className="question">WHO : Covid19 Home Page</h5>
           <a
             href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
             className="options-link"
@@ -94,9 +166,9 @@ function Options() {
           </a>
         </div>
         <div className="options fadeInUp" style={{ animationDelay: "0.7s" }}>
-          <h4 className="question">
+          <h5 className="question">
             Centers for Disease Control and Prevention
-          </h4>
+          </h5>
           <a
             href="https://www.cdc.gov/coronavirus/2019-ncov/faq.html"
             className="options-link"
@@ -105,7 +177,7 @@ function Options() {
           </a>
         </div>
         <div className="options fadeInUp" style={{ animationDelay: "0.75s" }}>
-          <h4 className="question">HELPLINE NUMBERS (by State)</h4>
+          <h5 className="question">HELPLINE NUMBERS (by State)</h5>
           <a
             href="https://www.mohfw.gov.in/pdf/coronvavirushelplinenumber.pdf"
             className="options-link"
@@ -113,10 +185,7 @@ function Options() {
             bit.ly/mohfw-helpline
           </a>
         </div>
-        <br id="line2" />
-        <br id="line2" />
-        <br id="line2" />
-        <br id="line2" />
+
         <Footer />
       </Container>
     </div>
