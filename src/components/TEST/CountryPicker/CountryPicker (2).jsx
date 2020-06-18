620ca141dfe5d73d7ed3eca25fba1574dd37696f
Dinesh Shaw <49403734+dinezh256@users.noreply.github.com>
2020-06-18 18:05:17 +0530

    revert commit

---
+++ src/components/TEST/CountryPicker/CountryPicker (2).jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+import ReactGa from "react-ga";
+import styles from "./CountryPicker.module.css";
+import { countries } from "../../API/index";
+
+const CountryPicker = ({ handleCountryChange }) => {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await countries());
+    };
+
+    fetchAPI();
+  }, [setFetchedCountries]);
+  if (fetchedCountries) {
+    return (
+      <FormControl
+        className={`${styles.formControl} fadeInUp`}
+        style={{ animationDelay: "1s" }}
+      >
+        <NativeSelect
+          defaultValue=""
+          onChange={(e) => handleCountryChange(e.target.value)}
+          onClick={ReactGa.event({
+            category: "Global options",
+            action: "Global options checked",
+          })}
+        >
+          <option value="Global">Global</option>
+          {fetchedCountries.map((item, i) => (
+            <option value={item.country} key={i}>
+              {item.country}
+            </option>
+          ))}
+        </NativeSelect>
+      </FormControl>
+    );
+  } else return null;
+};
+
+export default CountryPicker;
