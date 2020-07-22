0b45d8cd853474b362f37e5ca4c7ada0517ea920
Dinesh Shaw <49403734+dinezh256@users.noreply.github.com>
2020-07-23 00:18:37 +0530

    Add files via upload

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -29,123 +29,130 @@ const Footer = () => {
             <span className="hashTag">#DistancedButNotAlone</span>
           </h6>
         </a>
-        <a
-          href="https://instagram.com/covidindiastatsig"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ marginLeft: 2, marginRight: 2 }}
-        >
-          <button
-            className="btn"
-            style={{
-              textAlign: "center",
-              color: "white",
-              backgroundColor: "#E1306C",
-              marginTop: 5,
-              marginBottom: 15,
-              width: 38,
-              fontSize: 10,
-              alignContent: "center",
-              fontFamily: "notosans",
-            }}
-            onClick={() =>
-              ReactGa.event({
-                category: "Instagram Clicked",
-                action: "Instagram",
-              })
-            }
-          >
-            <Icon.Instagram size="15px" />
-          </button>
-        </a>
-        <a
-          href="https://facebook.com/covidindiastats"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ marginLeft: 2, marginRight: 2 }}
-        >
-          <button
-            className="btn"
-            style={{
-              textAlign: "center",
-              color: "white",
-              backgroundColor: "#3f729b",
-              marginTop: 5,
-              marginBottom: 15,
-              width: 38,
-              fontSize: 10,
-              alignContent: "center",
-              fontFamily: "notosans",
-            }}
-            onClick={() =>
-              ReactGa.event({
-                category: "Facebook button Clicked",
-                action: "Facebook",
-              })
-            }
-          >
-            <Icon.Facebook size="15px" />
-          </button>
-        </a>
-        <a
-          href="https://t.me/covidindiastats"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ marginLeft: 2, marginRight: 2 }}
-        >
-          <button
-            className="btn"
-            style={{
-              textAlign: "center",
-              color: "white",
-              backgroundColor: "rgb(0, 136, 204)",
-              marginTop: 5,
-              marginBottom: 15,
-              width: 38,
-              fontSize: 10,
-              alignContent: "center",
-              fontFamily: "notosans",
-            }}
-            onClick={() =>
-              ReactGa.event({
-                category: "Telegram Clicked",
-                action: "Telegram",
-              })
-            }
-          >
-            <Icon.Inbox size="15px" />
-          </button>
-        </a>
-        <a
-          href="https://twitter.com/covidindiastats"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ marginLeft: 2, marginRight: 2 }}
-        >
-          <button
-            className="btn"
-            style={{
-              textAlign: "center",
-              color: "white",
-              backgroundColor: "#00acee",
-              marginTop: 5,
-              marginBottom: 15,
-              width: 38,
-              fontSize: 10,
-              alignContent: "center",
-              fontFamily: "notosans",
-            }}
-            onClick={() =>
-              ReactGa.event({
-                category: "Twitter button Clicked",
-                action: "Twitter",
-              })
-            }
-          >
-            <Icon.Twitter size="15px" />
-          </button>
-        </a>
       </h6>
+      <div className="col">
+        <div align="center" className="col">
+          <a
+            href="https://instagram.com/covidindiaastats"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              className="btn"
+              style={{
+                textAlign: "center",
+                color: "rgb(255, 48, 108)",
+                backgroundColor: "rgba(225, 48, 108, 0.1)",
+                marginTop: 5,
+                width: 180,
+                fontSize: 10,
+                alignContent: "center",
+                fontFamily: "notosans",
+              }}
+              onClick={() =>
+                ReactGa.event({
+                  category: "Instagram Clicked",
+                  action: "Instagram",
+                })
+              }
+            >
+              <Icon.Instagram size="15px" />
+              &ensp; OUR INSTAGRAM PAGE
+            </button>
+          </a>
+        </div>
+        <div align="center" className="col">
+          <a
+            href="https://facebook.com/covidindiastats"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              className="btn"
+              style={{
+                textAlign: "center",
+                color: "rgb(63, 114, 195)",
+                backgroundColor: "rgba(63, 114, 155, 0.1)",
+                marginTop: 5,
+                width: 180,
+                fontSize: 10,
+                alignContent: "center",
+                fontFamily: "notosans",
+              }}
+              onClick={() =>
+                ReactGa.event({
+                  category: "Facebook button Clicked",
+                  action: "Facebook",
+                })
+              }
+            >
+              <Icon.Facebook size="15px" />
+              &ensp; OUR FACEBOOK PAGE
+            </button>
+          </a>
+        </div>
+        <div align="center" className="col">
+          <a
+            href="https://t.me/covidindiastats"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              className="btn"
+              style={{
+                textAlign: "center",
+                color: "rgb(0, 136, 234)",
+                backgroundColor: "rgba(0, 136, 204, 0.1)",
+                marginTop: 5,
+                width: 180,
+                fontSize: 10,
+                alignContent: "center",
+                fontFamily: "notosans",
+              }}
+              onClick={() =>
+                ReactGa.event({
+                  category: "Telegram Clicked",
+                  action: "Telegram",
+                })
+              }
+            >
+              <Icon.Inbox size="15px" />
+              &ensp; INSTANT UPDATES ON TELEGRAM
+            </button>
+          </a>
+        </div>
+        <div align="center" className="col">
+          <a
+            href="https://twitter.com/covidindiastats"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              className="btn"
+              style={{
+                textAlign: "center",
+                color: "rgb(0, 192, 248)",
+                backgroundColor: "rgba(0, 172, 238, 0.1)",
+                marginTop: 5,
+                marginBottom: 15,
+                width: 180,
+                fontSize: 10,
+                alignContent: "center",
+                fontFamily: "notosans",
+              }}
+              onClick={() =>
+                ReactGa.event({
+                  category: "Twitter button Clicked",
+                  action: "Twitter",
+                })
+              }
+            >
+              <Icon.Twitter size="15px" />
+              &ensp; UPDATES ON TWITTER
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
